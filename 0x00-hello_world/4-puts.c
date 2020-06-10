141f233fa25f26c6820389e66252478ba7ee7c1e
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-10 19:40:54 +0000

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void) /*C program that prints a string*/
+/**
+ * main - Prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle"); /*string*/
 return (0);
 }
