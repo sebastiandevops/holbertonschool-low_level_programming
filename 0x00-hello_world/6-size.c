a44e8f719fe8b72ba742fb837d881b07d03a8d1e
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-10 21:23:59 +0000

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,12 @@
  *
  * Return: 0
  */
-int main() 
-{ 
-printf("Size of char: %lu\n", sizeof(char)); 
+int main(void)
+{
+printf("Size of char: %lu\n", sizeof(char));
 printf("Size of an in: %lu\n", sizeof(int));
-printf("Size of a long int: %li\n", sizeof(long int)); 
-printf("Size of a long long int: %lli\n", sizeof(long long int));
-printf("Size of float: %lu\n", sizeof(float));  
-return 0; 
-} 
+printf("Size of a long int: %lu\n", sizeof(long int));
+printf("Size of a long long int: %lu\n", sizeof(long long int));
+printf("Size of float: %lu", sizeof(float));
+return (0);
+}
