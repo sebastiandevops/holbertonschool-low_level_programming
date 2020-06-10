79932f605f7fbff38fa170978feee5d6cf1d5bc1
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-10 20:19:22 +0000

    task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints "Size of diferent types"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of a char: 1 byte(s)");
+printf("Size of an int: 4 byte(s)");
+printf("Size of a long int: 8 byte(s)");
+printf("Size of a long long int: 8 byte(s)");
+printf("Size of a float: 4 byte(s)");
+
+return (0);
+}
