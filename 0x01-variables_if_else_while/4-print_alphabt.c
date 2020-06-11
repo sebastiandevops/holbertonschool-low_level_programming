4531d3d8d580d16db8688916d9a3bbe752d49ffd
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-11 23:32:22 +0000

    task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+/**
+ * main - Prints "program that prints the alphabet in lowercase"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
