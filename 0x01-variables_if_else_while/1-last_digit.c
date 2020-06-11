1e39b5a7356614177d8d2dc6ba9df4ba0236ee65
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-11 19:57:55 +0000

    task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %d is %d and is 0\n", n, ld);
 }
 else
 {
-printf("Last digit of %d is less than 6 and not 0\n", n);
+  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 }
 return (0);
 }
