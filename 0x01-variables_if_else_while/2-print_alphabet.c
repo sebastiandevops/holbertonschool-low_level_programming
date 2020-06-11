687529c86f58480390dc34c5ecc8dd328db39c1d
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-11 19:01:16 +0000

    task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints "Size of diferent"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+putchar(c);
+putchar('\n');
+return (0);
+}
