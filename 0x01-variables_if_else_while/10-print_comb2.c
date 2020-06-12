e75366caa1243285816ffe73ccd9c71d8dc79358
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-12 03:03:26 +0000

    task

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints "prints the numbers from 00 to 99"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100 ; i++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		if (i != 99)
+			putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
