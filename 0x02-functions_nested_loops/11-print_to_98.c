2a2c6009e8646edd8a7b9cc31dc827ce054105d2
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-16 22:32:40 +0000

    task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 /**
  * print_to_98 - return the sum of two integers
  * @n: number to start.
@@ -8,73 +7,22 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
 	if (n < 98)
 	{
-		for (i = n; i < 99 ; i++)
+		while (n < 98)
 		{
-			if (i < (-99))
-			{
-				_putchar('-');
-				i = -i;
-				_putchar((i / 100) + '0');
-				_putchar(((i - (i / 100) * 100) / 10) + '0');
-				_putchar((i % 10) + '0');
-				i = -i;
-			}
-			else if (i < (-9))
-			{
-				_putchar('-');
-				i = -i;
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				i = -i;
-			}
-			else if (i < 0)
-			{
-				_putchar('-');
-				i = -i;
-				_putchar(i + '0');
-				i = -i;
-			}
-			else if (i < 10)
-			{
-				_putchar(i + '0');
-			}
-			else if (i > 9)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d, ", n);
+			n++;
 		}
+
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
-		for (i = n; i > 98 ; i--)
+		while (n > 98)
 		{
-			if (i < 100)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else if (n >= 100)
-			{
-				_putchar((i / 100) + '0');
-				_putchar(((i - (i / 100) * 100) / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			if (i != 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d, ", n);
+			n--;
 		}
 	}
-	_putchar('\n');
+	printf("98\n");
 }
