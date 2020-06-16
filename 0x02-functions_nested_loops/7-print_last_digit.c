17a04826910b72e9c377968cb05d6e59a3639fe4
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-16 19:58:11 +0000

    task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,13 +4,13 @@
  * @n: The number to check
  *
  * Return: result.
- *
+ * Else: nothing
  */
 int print_last_digit(int n)
 {
 	int result;
 
-	if (n > 0)
+	if (n >= 0)
 	{
 		result = n % 10;
 		_putchar('0' + result);
