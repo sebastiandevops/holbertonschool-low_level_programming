e67362f8306014b3b03343c4c8a41737389d2bc1
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-16 18:36:52 +0000

    task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - return the sum of two integers
+ * @a: Integer to sum.
+ * @b: Integer to sum.
+ * Return: Result.
+ *
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = (a + b);
+	return (result);
+}
