4ea802a2e124f6ee7e6a5c55afa40bcbc0442551
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-18 21:49:33 +0000

    task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 != 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (n % 5 == 0 && n % 3 != 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (n == 1)
 		{
@@ -28,7 +28,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d ", n);
+			printf(" %d", n);
 		}
 		n++;
 	}
