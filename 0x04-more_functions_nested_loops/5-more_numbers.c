3519904dfee7f5aafbe19e61e1a785247ad30269
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-18 21:31:07 +0000

    task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,15 +15,11 @@ void more_numbers(void)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			if (c < 10)
-			{
-				_putchar(c + '0');
-			}
 			if (c > 9 && c < 15)
 			{
 				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
 			}
+			_putchar((c % 10) + '0');
 		}
 		i++;
 		_putchar('\n');
