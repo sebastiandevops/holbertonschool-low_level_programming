ae69ebf074ea59988d782796bf9af01b5e4c6b4b
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-18 18:15:01 +0000

    task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-int _putchar(char c);
+#include "holberton.h"
 /**
  * print_numbers - print numbers from 0 to 9
  *
@@ -11,8 +11,7 @@ void print_numbers(void)
 	int c;
 
 	for (c = '0'; c <= '9'; c++)
-	{
 		_putchar(c);
 		_putchar('\n');
-	}
+
 }
