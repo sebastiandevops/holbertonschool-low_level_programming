db4836922f6c57bc21082941d71ac0ea13cda308
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-18 13:39:23 +0000

    task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+int _putchar(char c);
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ *
+ * Return: void.
+ *
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
