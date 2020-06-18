5be537f38ae825e91a526f9d771aaf644d150817
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-18 19:58:32 +0000

    task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_square - function that draws a square in the terminal.
+ * @size: number of times that the line will be printed.
+ *
+ * Return: void.
+ *
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	/* Iterate through N rows */
+	for (i = 1; i <= size; i++)
+	{
+		/* Iterate over columns */
+		for (j = 1; j <= size; j++)
+		{
+			/* Print star for each column */
+			_putchar('#');
+		}
+
+		/* Move to the next line/row */
+		_putchar('\n');
+	}
+}
