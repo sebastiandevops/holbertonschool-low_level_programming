c79a303e099f6e7041936fb3e02a01d56bc3249c
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-18 21:19:53 +0000

    task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * print_triangle - function that draws a triange in the terminal.
+ * @size: size of the triangle that will be printed.
+ *
+ * Return: void.
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
