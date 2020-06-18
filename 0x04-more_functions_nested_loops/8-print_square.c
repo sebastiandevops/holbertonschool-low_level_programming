844dd26eaff98f664732ea744faace6ebd2dd555
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-18 20:13:01 +0000

    task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * print_square - function that draws a square in the terminal.
- * @size: number of times that the line will be printed.
+ * print_line - function that draws a straight line in the terminal.
+ * @n: number of times that the line will be printed.
  *
  * Return: void.
  *
@@ -10,20 +10,24 @@ void print_square(int size)
 {
 	int i, j;
 
-	/* Iterate through N rows */
-	for (i = 1; i <= size; i++)
-	{
-		/* Iterate over columns */
-		for (j = 1; j <= size; j++)
+	if (size < 0)
+        {
+		_putchar('\n');
+        }
+        else
+        {
+		/* Iterate through N rows */
+		for(i=1; i<=size; i++)
 		{
-			/* Print star for each column */
-			_putchar('#');
-		}
-		if (size < 0)
-		{
+			/* Iterate over columns */
+			for(j=1; j<=size; j++)
+			{
+				/* Print star for each column */
+				_putchar('#');
+			}
+			/* Move to the next line/row */
 			_putchar('\n');
 		}
-		/* Move to the next line/row */
-		_putchar('\n');
-	}
+        }
+
 }
