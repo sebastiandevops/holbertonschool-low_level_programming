a62f63fca4bd4fcebf27856b3377b74b918e2681
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-18 14:27:31 +0000

    task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+int _putchar(char c);
+/**
+ * print_most_numbers - print numbers trom 0 to 9 except 2, 4.
+ *
+ *
+ * Return: void.
+ *
+ */
+void print_most_numbers(void)
+{
+	int n = 1;
+
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+		n++;
+	}
+	_putchar('\n');
+}
