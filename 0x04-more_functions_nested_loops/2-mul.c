a793cb52dc4d7e2fa717332078f041f4c06efb45
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-18 13:14:22 +0000

    task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+ * mul - return the multiplication of two integers
+ * @a: Integer to mult.
+ * @b: Integer to mult.
+ * Return: Result.
+ *
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+	return (result);
+}
