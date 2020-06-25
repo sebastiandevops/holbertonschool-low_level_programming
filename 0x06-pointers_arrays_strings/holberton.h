f542a26a6fafe72e7f5084832a12afd4dff45412
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-25 15:02:52 +0000

    task

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -8,5 +8,6 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 int _strlen(char *s);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
 #endif /* HOLBERTON_H */
