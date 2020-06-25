73ec587fa5425c34944daf88fe63ec365546b97e
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-25 20:09:35 +0000

    task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0'; ++i)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] > s2[i])
-		{
-			return (15);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-15);
-		}
-		else
-		{
-			return (0);
-		}
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
 	return (0);
 }
