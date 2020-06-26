739de7f80896ebd4ef8d9499020451e66b247d1c
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-26 00:39:07 +0000

    task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * cap_string - function that capitalizes all words of a string..
+ * @s: string to change from lower to uppercase.
+ *
+ * Return: string.
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ',' || s[i] == ';' || s[i] == '.'
+        || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '('
+        || s[i] == ')' || s[i] == '{' || s[i] == '}' || s[i] == ' '
+		    || s[i] == '\t' || s[i] == '\n')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
