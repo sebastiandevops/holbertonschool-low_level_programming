097b0b254e6be6b4481b93073e7748109b80ef4c
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-26 04:54:55 +0000

    task

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,1 +1,48 @@
 #include "holberton.h"
+/**
+ * print_number - function that prints an integer..
+ * @n: number of times that the diagonal will be printed.
+ *
+ *
+ * Return: void.
+ *
+ */
+void print_number(int n)
+{
+	int cont;
+	int aux;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		aux = n;
+		cont = -1;
+		while (aux < -9)
+		{
+			aux = (aux / 10);
+			cont = (cont * 10);
+		}
+		while (cont <= -1)
+		{
+			_putchar((n / cont) + '0');
+			n = (n % cont);
+			cont = (cont / 10);
+		}
+	}
+	else
+	{
+		aux = n;
+		cont = 1;
+		while (aux > 9)
+		{
+			aux = (aux / 10);
+			cont = (cont * 10);
+		}
+		while (cont >= 1)
+		{
+			_putchar((n / cont) + '0');
+			n = (n % cont);
+			cont = (cont / 10);
+		}
+	}
+}
