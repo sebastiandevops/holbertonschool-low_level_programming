1bbc593c690d6345732590af47b14da11fcf35fe
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-06-29 20:52:56 +0000

    task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,20 +8,18 @@
  */
 char *_strchr(char *s, char c)
 {
-	char *position;
 	int i = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			position = &s[i];
 			break;
 		}
 	}
 	if (s[i] == c)
 	{
-		return (position);
+		return (&s[i]);
 	}
 	return (0);
 }
