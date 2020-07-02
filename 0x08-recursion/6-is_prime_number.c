d522ba4c02b1dacf309ae11447fdac9998ecb006
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-02 17:00:13 +0000

    task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,16 +9,8 @@
 
 int is_prime_number(int n)
 {
-	int i;
+	int i = 2;
 
-	if (n > 0)
-	{
-		i = 2;
-	}
-	else
-	{
-		i = -2;
-	}
 	return (foo_recursion(n, i));
 }
 
@@ -31,11 +23,11 @@ int is_prime_number(int n)
  */
 int foo_recursion(int n, int i)
 {
-	if (n == 1 || n == 0 || n == -1)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	else if (n > 0)
+	else
 	{
 		if (n % i == 0)
 		{
@@ -53,22 +45,5 @@ int foo_recursion(int n, int i)
 			i = i + 1;
 			return (foo_recursion(n, i));
 		}
-	}
-	else
-	{
-		if (n % i == 0)
-		{
-			if (i > n)
-			{
-				return (0);
-			}
-			else
-			{
-				return (1);
-			}
-		}
-		else
-			i = i - 1;
-			return (foo_recursion(n, i));
 	}
 }
