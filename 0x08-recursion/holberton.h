d13372cb5a4a2f490bc067b2380f56af4d4bfe98
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-02 00:59:52 +0000

    task

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -9,5 +9,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int is_prime_number(int n);
+int foo_recursion(int n, int i);
 
 #endif /* HOLBERTON_H */
