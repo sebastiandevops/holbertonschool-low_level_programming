ca7ac2ea0d2e441a7f1e22bfe920de8015544a7f
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-04 21:35:47 +0000

    task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,18 +9,18 @@
 int main(int argc, char *argv[])
 {
 	int i, mul = 1;
-
-	if (argc > 1)
+	if (argc <= 2)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			mul = mul * atoi(argv[i]);
-		}
-		printf("%d\n", mul);
+		printf("Error\n");
+		exit(1);
 	}
 	else
 	{
-		printf("Error\n");
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
 	}
 	return (0);
 }
