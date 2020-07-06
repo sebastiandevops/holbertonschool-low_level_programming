fb545ef9051ec75527b8f028b360fdebbb8a6e60
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-06 00:56:26 +0000

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-int isalpha(int argument);
+int isdigit(int argument);
+int _strlen(char *s);
 /**
  * main - program that adds positive numbers.
  * @argc: argument count.
@@ -9,20 +10,38 @@ int isalpha(int argument);
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) && atoi(argv[i]) > 0)
+		for (j = 0; argv[i] != '\0'
+		&& j < _strlen(argv[i]); j++)
 		{
-			sum += atoi(argv[i]);
+			if (isdigit(argv[i][j]) == 0)
+			{
+				printf("Error\n");
+				exit(1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			exit(1);
-		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
+}
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string to check.
+ *
+ * Return: length.
+ *
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+	}
+	return (i);
+
 }
