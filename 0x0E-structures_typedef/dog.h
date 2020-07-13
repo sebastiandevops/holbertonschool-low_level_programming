0c4551cd431e577836a829cb7c06b7b32b5adc65
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-13 14:39:47 -0500

    0. Django task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
