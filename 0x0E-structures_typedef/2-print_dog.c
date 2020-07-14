c31a36f8cd26c89307193ea0a61a70d02ae88dda
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-13 22:38:23 -0500

    attemp correction of else cases NULL values"

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,16 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", d->name);
-	printf("Age: %.1f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	else if (d->name == NULL)
+		printf("Name: (nil)");
+	else if (d->age == 0)
+		printf("Age: (nil)");
+	else if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %.1f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
 }
