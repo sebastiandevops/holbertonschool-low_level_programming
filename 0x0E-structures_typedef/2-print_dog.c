a907627d73c1bbe7d36360b2a94eb69e295738c3
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-13 22:23:03 -0500

    include correction when d == NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,7 +7,9 @@
  */
 void print_dog(struct dog *d)
 {
-		printf("Name: %s\n", d->name);
-		printf("Age: %.1f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+		return;
+	printf("Name: %s\n", d->name);
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
