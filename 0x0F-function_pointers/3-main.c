0ebfc4936311186e89b1780724684502da20b4dc
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-15 21:21:37 -0500

    test

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	get_op_func(char *s)(int, int);
+
+}
