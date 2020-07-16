86a1327be30d4fdbba5bea6abdb8f665e2283aaa
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-15 20:54:55 -0500

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,20 @@
+#include "3-calc.c"
+/**
+ * get_op_func - function that selects the correct
+ *function to perform the operation asked by the user.
+ * @s: operator passed as argument to the program.
+ * Return: Result.
+ *
+ */
+int (*get_op_func(char *s))(int, int)
+[
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+]
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include "3-calc.h"
+/**
+ * op_add - returns the sum of a and b.
+ * @a: Integer to sum.
+ * @b: Integer to sum.
+ * Return: Result.
+ *
+ */
+int op_add(int a, int b);
+{
+	int result;
+
+	result = (a + b);
+	return (result);
+}
+
+/**
+ * op_mul - returns the product of a and b.
+ * @a: Integer to mult.
+ * @b: Integer to mult.
+ * Return: Result.
+ *
+ */
+int op_mul(int a, int b);
+{
+	int result;
+
+	result = (a * b);
+	return (result);
+}
+
+/**
+ * op_sub - returns the difference of a and b.
+ * @a: Integer.
+ * @b: Integer.
+ * Return: Result.
+ *
+ */
+int op_sub(int a, int b);
+{
+	int result;
+
+	result = (a - b);
+	return (result);
+}
+
+/**
+ * op_div - returns the result of the division of a by b.
+ * @a: Integer.
+ * @b: Integer.
+ * Return: Result.
+ *
+ */
+int op_div(int a, int b);
+{
+	int result;
+
+	result = (a / b);
+	return (result);
+}
+
+/**
+ * op_mod - returns the remainder of the division of a by b.
+ * @a: Integer.
+ * @b: Integer.
+ * Return: Result.
+ *
+ */
+int op_mod(int a, int b);
+{
+	int result;
+
+	result = (a % b);
+	return (result);
+}
