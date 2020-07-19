6ebff341c9ba704636287dd3753b264aabf4ebc1
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-19 14:00:32 -0500

    Modification of the function to avoid to pass more than 40 lines

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,67 +9,43 @@
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	char *s;
-	int i;
-	int j = 0;
+	char *s, c;
+	int i, j, n = 0;
 	double d;
-	char c;
-	int n = 0;
-
-	while (format[n] != '\0')
-		n++;
 
 	va_start(valist, format);
+	while (format[n] != '\0')
+		n++;
 	for (j = 0; j < n; j++)
 	{
-
-		if (j == n - 1)
+		if (format[j] == 'c')
 		{
-			if (format[j] == 'c')
-			{
-				c = va_arg(valist, int);
-				printf("%c\n", c);
-			}
-			else if (format[j] == 'i')
-			{
-				i = va_arg(valist, int);
-				printf("%d\n", i);
-			}
-			else if (format[j] == 'f')
-			{
-				d = va_arg(valist, double);
-				printf("%f\n", d);
-			}
-			else if (format[j] == 's')
-			{
-				s = va_arg(valist, char*);
-				printf("%s\n", s);
-			}
+			c = va_arg(valist, int);
+			printf("%c", c);
+		}
+		else if (format[j] == 'i')
+		{
+			i = va_arg(valist, int);
+			printf("%d", i);
+		}
+		else if (format[j] == 'f')
+		{
+			d = va_arg(valist, double);
+			printf("%f", d);
+		}
+		else if (format[j] == 's')
+		{
+			s = va_arg(valist, char*);
+			printf("%s", s);
 		}
 		else
-		{
-			if (format[j] == 'c')
-			{
-				c = va_arg(valist, int);
-				printf("%c, ", c);
-			}
-			else if (format[j] == 'i')
-			{
-				i = va_arg(valist, int);
-				printf("%d, ", i);
-			}
-			else if (format[j] == 'f')
-			{
-				d = va_arg(valist, double);
-				printf("%f, ", d);
-			}
-			else if (format[j] == 's')
-			{
-				s = va_arg(valist, char*);
-				printf("%s, ", s);
-			}
-		}
+			continue;
+
+		if (j == n - 1)
+			printf("\n");
+		else
+			printf(", ");
+
 	}
 	va_end(valist);
 }
-
