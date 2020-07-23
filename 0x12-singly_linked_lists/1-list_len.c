fb491992a22f7ee81fe4942e378b45ab208a0521
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-23 14:51:31 -0500

    first two task of linked lists project

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - function that returns the number of elements in a linked list.
+ * @h: list header.
+ *
+ * Return: number of elements.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
