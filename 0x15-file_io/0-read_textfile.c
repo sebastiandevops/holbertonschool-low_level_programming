918a4cd1bc4cc2b9c841091c40deee01b71764df
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-08-04 17:53:46 -0500

    task 0-read_textfile.c, second attempt

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int count = 0;
+	int ret;
 	char *buf = malloc(letters + 1);
 
 	fd = open(filename, O_RDONLY);
@@ -19,25 +19,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	read(fd, buf, letters);
-	while (*buf)
-	{
-		_putchar(*buf);
-		buf++;
-		count++;
-	}
+	ret = read(fd, buf, letters);
+	if (ret == -1)
+		return (0);
+	write(STDOUT_FILENO, buf, letters);
 	close(fd);
-	return (count);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
+	return (ret);
 }
