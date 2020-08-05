76d486d93b9e2522b0035b344aa8e371394e1bf2
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-08-04 22:38:57 -0500

    fixing number 3 task: adding file permissions

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -13,8 +13,8 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: %s filename text\n", av[0]);
-		exit(1);
+		dprintf(2, "Usage: %s %s\n", av[1], av[2]);
+		exit(97);
 	}
 	res = create_file(av[1], av[2]);
 	printf("-> %i)\n", res);
