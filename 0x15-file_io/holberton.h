ecefa80d13fe244dc64c1b562b12fc22b5599583
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-08-04 22:24:29 -0500

    update of holberton.h header file

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -13,5 +13,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
 int append_text_to_file(const char *filename, char *text_content);
+int read_textfile2(const char *filename, const char *copyname, size_t letters);
 
 #endif /* HOLBERTON_H */
