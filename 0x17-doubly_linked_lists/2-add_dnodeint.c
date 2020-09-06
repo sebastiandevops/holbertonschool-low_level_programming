1e7bc3fb6ddb5040175f1163c4ce70907567f14c
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-06 16:43:47 -0500

    3rd task

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_dnodeint - function that adds a new node at the beginning
+ * of a dlistint_t list.
+ * @head: list header.
+ * @n: integer.
+ * Return: new node.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	int temp = n;
+
+	new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = temp;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string to check.
+ *
+ * Return: length.
+ *
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+	}
+	return (i);
+}
