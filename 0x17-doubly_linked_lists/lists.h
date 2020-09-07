d6f52acf231922efc23324bc84f385ff0f610144
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-09-06 20:22:07 -0500

    fixing errors on header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -16,9 +16,9 @@ int _putchar(char c);
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
