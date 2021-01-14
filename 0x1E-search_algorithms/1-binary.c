b061754d81bb5cee69d2ffc4676663bef3b6f001
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-01-14 16:16:47 -0500

    2nd task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,6 @@
 #include "search_algos.h"
-
 /**
- * linear_search - searches for a value in an array of integers using
+ * binary_search - searches for a value in an array of integers using
  * the Binary search algorithm.
  * @array: pointer to the first element of the array to search in.
  * @size: number of elements in array.
@@ -11,5 +10,83 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t l = 0;
+	size_t r = size - 1;
 
+	while (l <= r)
+	{
+		size_t m = l + (r - l) / 2;
+
+		if (array[m] == value)
+		{
+			size_t j;
+
+			printf("Searching in array: ");
+			for (j = l; j < r + 1; j++)
+			{
+				if (j != r)
+					printf("%d, ", array[j]);
+				else
+					printf("%d", array[j]);
+			}
+			printf("\n");
+			return (m);
+		}
+		if (array[m] < value)
+		{
+			printR(array, r, l);
+			l = m + 1;
+		}
+		else
+		{
+			printL(array, r);
+			r = m - 1;
+		}
+	}
+	return (-1);
+}
+
+/**
+ * printL - prints left side.
+ * @array: pointer to the first element of the array to print.
+ * @r: right limit of array.
+ *
+ * Return: void.
+ */
+void printL(int *array, size_t r)
+{
+	size_t j;
+
+	printf("Searching in array: ");
+	for (j = 0; j < r + 1; j++)
+	{
+		if (j != r)
+			printf("%d, ", array[j]);
+		else
+			printf("%d", array[j]);
+	}
+	printf("\n");
+}
+
+/**
+ * printR - prints right side of array.
+ * @array: pointer to the first element of the array to print.
+ * @r: right limit of array.
+ * @l: left limit of array.
+ *
+ * Return: void.
+ */
+void printR(int *array, size_t r, size_t l)
+{
+	size_t j;
+
+	printf("Searching in array: ");
+	for (j = l; j < r + 1; j++)
+	{
+		if (j != r)
+			printf("%d, ", array[j]);
+		else
+			printf("%d", array[j]);
+	}
+	printf("\n");
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void printL(int *array, size_t r);
+void printR(int *array, size_t r, size_t l);
 
 #endif /* SEARCH_ALGOS */
